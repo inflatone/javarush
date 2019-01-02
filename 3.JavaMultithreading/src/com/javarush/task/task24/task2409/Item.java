c33c0086c076e33db187afbdd2334bc817f4ff2e
sane5ever <sane5ever@gmail.com>
2019-01-02 19:29:03 +0400

    com.javarush.task.task24.task2409

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2409/Item.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task24.task2409;
+
+/**
+ * Вещь.
+ *
+ * @author Alexander Savchenko
+ * @version 1.0
+ * @since 2019-01-02
+ */
+public interface Item {
+    int getId();
+    double getPrice();
+    String getTM();
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2409/Jeans.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task24.task2409;
+
+/**
+ * Джинсы.
+ *
+ * @author Alexander Savchenko
+ * @version 1.0
+ * @since 2019-01-02
+ */
+public interface Jeans extends Item {
+    int getLength();
+    int getSize();
+}
