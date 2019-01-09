82c1629a6325972f8aa98a8850b6321ca8522d8f
sane5ever <sane5ever@gmail.com>
2019-01-09 21:24:59 +0400

    --bulbs blinking by rotation--{UPDATED}

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2309/vo/Main.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2309/vo/Main.java
@@ -1,10 +1,7 @@
 package com.javarush.task.task23.task2309.vo;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-import static java.lang.Thread.currentThread;
-import static java.lang.Thread.sleep;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 
 /**
  * Последовательно работающие лампочки.
@@ -14,43 +11,49 @@ import static java.lang.Thread.sleep;
  * @since 2018-12-29
  */
 public class Main {
+    public final static int AMOUNT = 10;
+
+
     public static void main(String[] args) {
-        Runnable first = new Bulb("first");
-        Runnable second = new Bulb("second");
-
-        ExecutorService service = Executors.newSingleThreadExecutor();
-        for (int i = 0; i < 100; i++) {
-            service.submit(first);
-            service.submit(second);
-        }
-
-
+        Object lock = new Object();
+        AtomicInteger counter = new AtomicInteger();
+        IntStream.range(0, AMOUNT)
+                .mapToObj(i -> new Bulb(String.format("Bulb #%s", i), i, counter, lock))
+                .forEach(Thread::start);
     }
 }
 
-class Bulb implements Runnable {
-
+class Bulb extends Thread {
     private final String name;
+    private final Object lock;
+    private final int num;
+    private final AtomicInteger counter;
 
 
-
-    Bulb(String name) {
+    Bulb(String name, int num, AtomicInteger counter, Object lock) {
         this.name = name;
-
+        this.num = num;
+        this.counter = counter;
+        this.lock = lock;
     }
 
     public void run() {
-        Thread self = currentThread();
-
-
-        System.out.println(name + " bulb is on");
-        try {
-            sleep(1000);
-        } catch (InterruptedException e) {
-            self.interrupt();
+        Thread self = Thread.currentThread();
+        while (!self.isInterrupted()) {
+            synchronized (lock) {
+                try {
+                    while (counter.get() % Main.AMOUNT != num) {
+                        lock.wait();
+                    }
+                    System.out.println(name + " bulb is on");
+                    sleep(1000);
+                } catch (InterruptedException e) {
+                    self.interrupt();
+                }
+                System.out.println(name + " bulb is off");
+                counter.incrementAndGet();
+                lock.notifyAll();
+            }
         }
-        System.out.println(name + " bulb is off");
-
-
     }
 }
