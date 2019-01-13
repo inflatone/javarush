931ac8e660009e30c982d082157d273317f9e230
sane5ever <sane5ever@gmail.com>
2019-01-13 17:17:35 +0400

    com.javarush.task.task30.task3003

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3013/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3013/Solution.java
@@ -7,7 +7,7 @@ package com.javarush.task.task30.task3013;
 public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int number = Integer.MAX_VALUE - 133;
+        int number = Integer.MAX_VALUE;
         System.out.println(Integer.toString(number, 2));
 
         String result = Integer.toString(solution.resetLowerBits(number), 2);
