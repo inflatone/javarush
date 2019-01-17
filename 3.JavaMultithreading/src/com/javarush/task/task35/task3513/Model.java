e349c604f1092d9b786c6af1323847530a30b3cc
sane5ever <sane5ever@gmail.com>
2019-01-17 18:26:01 +0400

    com.javarush.task.task35.task3513: compressTiles() + mergeTiles()

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 public class Model {
     private final static int FIELD_WIDTH = 4;
+    int score, maxTile;
     private Tile[][] gameTiles;
 
     public Model() {
@@ -21,6 +22,8 @@ public class Model {
         }
         addTile();
         addTile();
+        score = 0;
+        maxTile = 0;
     }
 
     private void addTile() {
@@ -37,4 +40,52 @@ public class Model {
                 .filter(tile -> tile.value == 0)
                 .collect(Collectors.toList());
     }
+
+    private void compressTiles(Tile[] tiles) {
+        for (int i = 0, offset = 0; i < tiles.length; i++) {
+            if (tiles[i].value == 0) {
+                offset++;
+            } else if (offset != 0) {
+                swap(tiles[i - offset], tiles[i]);
+            }
+        }
+    }
+
+    private void mergeTiles(Tile[] tiles) {
+        for (int i = 1, offset = 1; i < tiles.length && tiles[i].value != 0; i++) {
+            if (tiles[i - offset].value == tiles[i].value) {
+                tiles[i - offset].value *= 2;
+                tiles[i].value = 0;
+                updateScore(tiles[i - offset]);
+                offset++;
+                i++;
+            }
+            if (offset > 1) {
+                shiftBackCurrentTile(tiles, i, offset);
+            }
+        }
+    }
+
+    private void shiftBackCurrentTile(Tile[] tiles, int i, int offset) {
+        if (i != tiles.length) {
+            tiles[i - offset + 1].value = tiles[i].value;
+            tiles[i].value = 0;
+        } else {
+            tiles[i - offset + 1].value = 0;
+        }
+    }
+
+    private void updateScore(Tile tile) {
+        score += tile.value;
+        if (maxTile < tile.value) {
+            maxTile = tile.value;
+        }
+    }
+
+    private void swap(Tile a, Tile b) {
+        a.value ^= b.value;
+        b.value ^= a.value;
+        a.value ^= b.value;
+    }
+
 }
