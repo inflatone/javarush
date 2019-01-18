b3859a6f8644154a805c031327f32e06e83229c7
sane5ever <sane5ever@gmail.com>
2019-01-18 23:47:01 +0400

    com.javarush.task.task35.task3513: MenuHelper

---
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/actions/SubscriptAction.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task32.task3209.actions;
+
+/**
+ * TODO: comment
+ *
+ * @author Alexander Savchenko
+ * @version 1.0
+ * @since 2019-01-19
+ */
+public class SubscriptAction {
+}
