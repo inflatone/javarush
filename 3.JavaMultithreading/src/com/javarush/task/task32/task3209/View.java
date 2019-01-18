7c5b05d621add5a4717e16a098c4f700aa485fab
sane5ever <sane5ever@gmail.com>
2019-01-18 23:29:37 +0400

    com.javarush.task.task32.task3209: Controller + View skeleton

---
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -0,0 +1,55 @@
+package com.javarush.task.task32.task3209;
+
+import com.javarush.task.task32.task3209.listeners.FrameListener;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class View extends JFrame implements ActionListener {
+    private Controller controller;
+    private JTabbedPane tabbedPane = new JTabbedPane();
+    private JTextPane htmlTextPane = new JTextPane();
+    private JEditorPane plainTextPane = new JEditorPane();
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
+
+    public Controller getController() {
+        return controller;
+    }
+
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+
+    public void init() {
+        initGui();
+        FrameListener listener = new FrameListener(this);
+        this.addWindowListener(listener);
+        this.setVisible(true);
+    }
+
+    public void exit() {
+        controller.exit();
+    }
+
+    public void initMenuBar() {
+
+    }
+
+    public void initEditor() {
+
+    }
+
+    public void initGui() {
+        initMenuBar();
+        initEditor();
+        pack();
+    }
+
+    public void selectedTabChanged() {
+
+    }
+}
