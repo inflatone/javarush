bc4398800985767c0ae124f142e5e9d4e693ec24
sane5ever <sane5ever@gmail.com>
2019-01-20 18:09:39 +0400

    com.javarush.task.task31.task3110: walking the file tree algorithm

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,21 +1,28 @@
 package com.javarush.task.task31.task3110;
 
-import com.javarush.task.task31.task3110.command.ExitCommand;
+import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.nio.file.Paths;
+import java.io.IOException;
 
 public class Archiver {
     public static void main(String[] args) throws Exception {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Absolute path of archive?");
-        ZipFileManager manager = new ZipFileManager(Paths.get(reader.readLine()));
-        System.out.println("Absolute path of archived items?");
-        manager.createZip(Paths.get(reader.readLine()));
+        for (Operation operation = null; operation != Operation.EXIT; ) {
+            try {
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+            } catch (WrongZipFileException e) {
+                ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
+            } catch (Exception e) {
+                ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
+            }
+        }
+    }
 
-        new ExitCommand().execute();
-
-
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage("Выберите операцию:");
+        for (Operation operation : Operation.values()) {
+            ConsoleHelper.writeMessage(String.format("%s - %s", operation.ordinal(), operation.getDescription()));
+        }
+        return Operation.values()[ConsoleHelper.readInt()];
     }
 }
