f4208156451187327798fc17d44f1bad04cf93b4
sane5ever <sane5ever@gmail.com>
2019-01-28 13:40:59 +0400

    com.javarush.task.task35.task3505

---
+++ 4.JavaCollections/src/com/javarush/task/task35/task3505/ConvertableUtil.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task35.task3505;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class ConvertableUtil {
+
+    public static <K, V extends Convertable<K>> Map<K, V> convert(List<V> list) {
+        return list.stream().collect(Collectors.toMap(Convertable::getKey, Function.identity()));
+    }
+}
