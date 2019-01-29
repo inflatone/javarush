3ca0515e156fb822d56b85d34da6708480e74fbd
sane5ever <sane5ever@gmail.com>
2019-01-29 20:31:16 +0400

    com.javarush.task.task39.task3913: QLQuery implementation

--- 4.JavaCollections/src/com/javarush/task/task39/task3913/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task39/task3913/Solution.java
@@ -5,6 +5,6 @@ import java.nio.file.Paths;
 public class Solution {
     public static void main(String[] args) {
         LogParser logParser = new LogParser(Paths.get("D:\\YandexDisk\\!!javarush\\JavaRushTasks\\4.JavaCollections\\src\\com\\javarush\\task\\task39\\task3913\\logs"));
-        System.out.println(logParser.getAllSolvedTasksAndTheirNumber(null, null));
+        System.out.println(logParser.execute("get ip"));
     }
 }
