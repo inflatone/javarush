5e624a26673434ba419ebdb167ebb9c92def15c4
sane5ever <sane5ever@gmail.com>
2019-01-30 20:02:37 +0400

    com.javarush.task.task39.task3913: query w/ date implementation

--- 4.JavaCollections/src/com/javarush/task/task39/task3913/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task39/task3913/Solution.java
@@ -6,7 +6,7 @@ public class Solution {
     public static void main(String[] args) {
         LogParser logParser = new LogParser(Paths.get("D:\\YandexDisk\\!!javarush\\JavaRushTasks\\4.JavaCollections\\src\\com\\javarush\\task\\task39\\task3913\\logs"));
         long start = System.currentTimeMillis();
-        System.out.println(logParser.execute("get date"));
+        System.out.println(logParser.execute("get event for user = \"Eduard Petrovich Morozko\" and date between \"11.12.2013 0:00:00\" and \"03.01.2014 23:59:59\""));
         System.out.println(System.currentTimeMillis() - start);
     }
 }
