2eb4fa7006e89da2436f59ea0aed0ac996239215
sane5ever <sane5ever@gmail.com>
2019-01-30 15:44:57 +0400

    com.javarush.task.task39.task3913: query w/ param implementation

--- 4.JavaCollections/src/com/javarush/task/task39/task3913/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task39/task3913/Solution.java
@@ -5,6 +5,8 @@ import java.nio.file.Paths;
 public class Solution {
     public static void main(String[] args) {
         LogParser logParser = new LogParser(Paths.get("D:\\YandexDisk\\!!javarush\\JavaRushTasks\\4.JavaCollections\\src\\com\\javarush\\task\\task39\\task3913\\logs"));
-        System.out.println(logParser.execute("get ip"));
+        long start = System.currentTimeMillis();
+        System.out.println(logParser.execute("get date"));
+        System.out.println(System.currentTimeMillis() - start);
     }
 }
