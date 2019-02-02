4359363b98fc3cfb170fc908beaa1e7b18cc813b
sane5ever <sane5ever@gmail.com>
2019-02-02 14:03:14 +0400

    com.javarush.task.task28.task2810: compose vacancies.html output

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
@@ -2,20 +2,24 @@ package com.javarush.task.task28.task2810.view;
 
 import com.javarush.task.task28.task2810.Controller;
 import com.javarush.task.task28.task2810.vo.Vacancy;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public class HtmlView implements View {
-    private Controller controller;
-
     private final String filePath =
             "./4.JavaCollections/src/"
                     + getClass().getPackage().getName().replace('.', '/')
-                    + "/vacancies.html"
-    ;
+                    + "/vacancies.html";
+    private Controller controller;
 
     @Override
     public void update(List<Vacancy> vacancies) {
@@ -41,14 +45,46 @@ public class HtmlView implements View {
         controller.onCitySelect("Odessa");
     }
 
+    protected Document getDocument() throws IOException {
+        return Jsoup.parse(
+                new File(filePath),
+                StandardCharsets.UTF_8.name()
+        );
+    }
+
     private String getUpdatedFileContent(List<Vacancy> vacancies) {
-        return "";
+        String result;
+        try {
+            Document original = getDocument();
+            Element originalTemplateRef = original.getElementsByClass("template").first();
+            Element template = originalTemplateRef.clone();
+            template.removeAttr("style");
+            template.removeClass("template");
+            original.getElementsByClass("vacancy").stream()
+                    .filter(e -> !"vacancy template".equals(e.className()))
+                    .forEach(Node::remove);
+
+            for (Vacancy v : vacancies) {
+                Element current = template.clone();
+                current.getElementsByClass("city").first().text(v.getCity());
+                current.getElementsByClass("companyName").first().text(v.getCompanyName());
+                current.getElementsByClass("salary").first().text(v.getSalary());
+                current.select("a").first()
+                        .text(v.getTitle())
+                        .attr("href", v.getUrl());
+                originalTemplateRef.before(current);
+            }
+            result = original.outerHtml();
+        } catch (IOException e) {
+            e.printStackTrace();
+            result = "Some exception occurred";
+        }
+        return result;
     }
 
     private void updateFile(String content) throws IOException {
         OutputStream out = new FileOutputStream(filePath);
         out.write(content.getBytes());
         out.close();
-
     }
 }
