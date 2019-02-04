06adede612d71fa942e2594534731061b5621472
sane5ever <sane5ever@gmail.com>
2019-02-04 18:24:09 +0400

    com.javarush.task.task26.task2613: localization from ResourceBundle

--- 4.JavaCollections/src/com/javarush/task/task26/task2613/command/LoginCommand.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/command/LoginCommand.java
@@ -1,27 +1,37 @@
 package com.javarush.task.task26.task2613.command;
 
+import com.javarush.task.task26.task2613.CashMachine;
 import com.javarush.task.task26.task2613.ConsoleHelper;
 import com.javarush.task.task26.task2613.exception.InterruptOperationException;
 
+import java.util.ResourceBundle;
+
 public class LoginCommand implements Command {
-    private static final String CARD_NUMBER = "123456789012";
-    private static final String CARD_PIN = "1234";
+    private ResourceBundle validCreditCards
+            = ResourceBundle.getBundle(CashMachine.class.getPackage().getName() + ".resources.verifiedCards");
 
     @Override
     public void execute() throws InterruptOperationException {
         String card;
         String pin;
-        while (true) {
+        do {
             ConsoleHelper.writeMessage("Card number?");
             card = ConsoleHelper.readString();
             ConsoleHelper.writeMessage("Pin number?");
             pin = ConsoleHelper.readString();
-            if (!card.matches(ConsoleHelper.CARD_NUMBER_REGEX) || !pin.matches(ConsoleHelper.PIN_REGEX)) {
-                continue;
-            }
-            if (card.equals(CARD_NUMBER) && pin.equals(CARD_PIN)) {
-                break;
-            }
+        } while (isIncorrect(card, pin));
+    }
+
+    private boolean isIncorrect(String card, String pin) {
+        if (!card.matches(ConsoleHelper.CARD_NUMBER_REGEX) || !pin.matches(ConsoleHelper.PIN_REGEX)) {
+            ConsoleHelper.writeMessage("incorrect data format");
+            return true;
         }
+        if (!validCreditCards.containsKey(card) || !validCreditCards.getString(card).equals(pin)) {
+            ConsoleHelper.writeMessage("inactive card or wrong pin");
+            return true;
+        }
+        ConsoleHelper.writeMessage("successful authorization");
+        return false;
     }
 }
