c12d1e0c51f8bc7867afc55ce5ece7860656e532
sane5ever <sane5ever@gmail.com>
2019-02-07 17:31:42 +0400

    com.javarush.task.task37.task3711

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3711/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task37.task3711;
+
+import java.util.concurrent.*;
+
+/*
+Фасад
+*/
+public class Solution {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        new Computer().run();
+    }
+}
