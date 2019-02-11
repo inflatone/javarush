d8b687fd5c20a71363840df541756c449342aa7a
sane5ever <sane5ever@gmail.com>
2019-02-11 18:57:55 +0400

    com.javarush.task.task40.task4006

--- 4.JavaCollections/src/com/javarush/task/task40/task4006/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task40/task4006/Solution.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.net.HttpURLConnection;
 import java.net.Socket;
 import java.net.URL;
 
@@ -19,17 +18,23 @@ public class Solution {
 
     public static void getSite(URL url) {
         try {
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            Socket socket = new Socket(url.getHost(), 80);
+            PrintStream out = new PrintStream(socket.getOutputStream());
+            System.out.printf("GET %s HTTP/1.1%n", url.getPath());
 
-            connection.setRequestMethod("GET");
+            out.printf("GET %s HTTP/1.1%nHost: %s%n%n", url.getPath(), url.getHost());
+            out.flush();
 
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             String responseLine;
 
             while ((responseLine = bufferedReader.readLine()) != null) {
+                System.out.println("!!");
                 System.out.println(responseLine);
             }
+            out.close();
             bufferedReader.close();
+            socket.close();
 
         } catch (IOException e) {
             e.printStackTrace();
