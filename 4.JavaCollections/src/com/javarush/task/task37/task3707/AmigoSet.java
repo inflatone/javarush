452d40119a121682aa6acb38a527ed3b30e1b7ba
sane5ever <sane5ever@gmail.com>
2019-03-06 20:03:27 +0400

    com.javarush.task.task37.task3707: constructors + add()

--- 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -1,15 +1,30 @@
 package com.javarush.task.task37.task3707;
 
 import java.io.Serializable;
-import java.util.AbstractSet;
-import java.util.Iterator;
-import java.util.Set;
-import java.util.Spliterator;
+import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class AmigoSet<E> extends AbstractSet<E> implements Serializable, Cloneable, Set<E> {
+    private static final Object PRESENT = new Object();
+
+    private transient HashMap<E, Object> map;
+
+    public AmigoSet() {
+        map = new HashMap<>();
+    }
+
+    public AmigoSet(Collection<? extends E> collection) {
+        map = new HashMap<>(Math.max((int) (collection.size()/.75f) + 1, 16));
+        addAll(collection);
+    }
+
+    @Override
+    public boolean add(E e) {
+        return map.put(e, PRESENT) == null;
+    }
+
     @Override
     public Iterator<E> iterator() {
         return null;
