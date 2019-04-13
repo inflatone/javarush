9397f65c7092ae75d1564f7b3d1d89525a2274a5
sane5ever <sane5ever@gmail.com>
2019-04-13 09:00:11 +0400

    com.javarush.task.task34.task3410: game objects storage

---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/GameObjects.java
@@ -0,0 +1,41 @@
+package com.javarush.task.task34.task3410.model;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class GameObjects {
+    private Set<Wall> walls;
+    private Set<Box> boxes;
+    private Set<Home> homes;
+    private Player player;
+
+    public GameObjects(Set<Wall> walls, Set<Box> boxes, Set<Home> homes, Player player) {
+        this.walls = walls;
+        this.boxes = boxes;
+        this.homes = homes;
+        this.player = player;
+    }
+
+    public Set<Wall> getWalls() {
+        return walls;
+    }
+
+    public Set<Box> getBoxes() {
+        return boxes;
+    }
+
+    public Set<Home> getHomes() {
+        return homes;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public Set<GameObject> getAll() {
+        return Stream.of(walls, boxes, homes, Collections.singleton(player))
+                .flatMap(Collection::stream)
+                .collect(Collectors.toSet());
+    }
+}
